9dd74f5a6f2220af30f9f4caab42c746f1b98fdc
kter <me@kter.jp>
2016-01-10 12:17:58 +0900

    ディレクトリ名に依存しないようにした

--- init.sh
+++ init.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-ln -fs $HOME/settings/.gitignore $HOME/
-ln -fs $HOME/settings/.gvimrc $HOME/
-ln -fs $HOME/settings/.irbrc $HOME/
-ln -fs $HOME/settings/.tmux.conf $HOME/
-ln -fs $HOME/settings/.vimrc $HOME/
-ln -fs $HOME/settings/.vimrc_evervim.sample $HOME/
-ln -fs $HOME/settings/.zsh/ $HOME/
-ln -fs $HOME/settings/.zshrc $HOME/
-ln -fs $HOME/settings/.zshrc.local $HOME/
+ln -fs .gitignore $HOME/
+ln -fs .gvimrc $HOME/
+ln -fs .irbrc $HOME/
+ln -fs .tmux.conf $HOME/
+ln -fs .vimrc $HOME/
+ln -fs .vimrc_evervim.sample $HOME/
+ln -fs .zsh/ $HOME/
+ln -fs .zshrc $HOME/
+ln -fs .zshrc.local $HOME/
 
 mkdir -p $HOME/.vim/undo
 mkdir -p $HOME/.vim/tmp
